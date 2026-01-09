e3bf0b25395877a3a237f61e11b8789f90232825
= <apritam2061@gmail.com>
2026-01-09 20:50:21 +0545

    "something changed"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
-import Main from './Components/Main/main'
+import Main from './Components/Main/Main'
 
 const App = () => {
   return (
